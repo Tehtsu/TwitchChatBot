b9c44385c785e75b2bd3e85876e2329fe6226484
Timme <movinggames@outlook.de>
2022-06-23 10:19:55 +0200

    friends command implemented

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const weatherApiBaseUrl = 'https://api.openweathermap.org';
 
 let helpLoopInterval;
 let socialLoopInterval;
+let tagIsSet = false
 const substr = ' ';
 
 let randomNumber = Math.floor(Math.random() * 20);
@@ -117,7 +118,42 @@ client.connect().then(() => {
             }
         }
 
+        /**
+         * TAG 'Mit Zuschauern zusammen spielen'
+         */
         if (command === 'friends'){
+            try{
+            const [username] = args;
+                const userUrl = `${twitchApiBaseUrl}/users?login=${username}`;
+
+                const headers = {
+                    headers: {
+                        'Client-Id': process.env.CLIENT_ID,
+                        Authorization: `Bearer ${process.env.APP_TOKEN}`
+                    },
+                };
+
+                const repsonseID = await axios.get(userUrl, headers);
+                const userId = repsonseID.data.data[0].id;
+
+                const getTagslUrl = `${twitchApiBaseUrl}/streams/tags?broadcaster_id=${userId}`;
+                const responseTags = await axios.get(getTagslUrl, headers);
+                let tagID = responseTags.data.data;
+                tagID.filter((t) => {
+                    if (t.tag_id === 'ac763b17-7bea-4632-9eb4-d106689ff409') {
+                        tagIsSet = true;
+                    } else {
+                        tagIsSet = false;
+                    }
+                })
+                if (tagIsSet === true) {
+                    client.say(channel, 'Steam: 66922904 || Battle.net: TetsuyaGames#2176 || EPIC: TetsuyaGames');
+                } else {
+                    client.say(channel, `@${tags['display-name']}, heute kannst du leider nicht mitspielen.`);
+                }
+            } catch (err){
+                console.log(err);
+            }
 
         }
 
