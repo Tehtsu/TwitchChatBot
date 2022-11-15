873a3db53560b1c554e5aa56d9dd60b9e25cd7fd
Timme <movinggames@outlook.de>
2022-11-15 22:50:17 +0100

    streamer updatet

---
+++ commands/coms.js
@@ -0,0 +1,8 @@
+module.exports = {
+    alias: ['communitys', 'com', 'coms'],
+    async execute(client, channel, args, tags) {
+        client.say(channel, 'Checkt doch mal die SGC aus -> https://soestgaming.com/.')
+        client.say(channel, 'Checkt doch mal die FaF aus -> https://linktr.ee/fafdiscordundco.')
+
+    }
+}
